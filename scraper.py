5a5d290fa009d6a79b6f916d8b3ac3d41af34f81
Oscar Mark <hmark@uci.edu>
2023-04-26 09:19:55 -0700

    finished tokenize function

--- scraper.py
+++ scraper.py
@@ -2,6 +2,35 @@ import re
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
+import requests
+from bs4 import BeautifulSoup
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def tokenize(url):
+    # Retrieve HTML content from the URL
+    response = requests.get(url)
+    html_content = response.text
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Extract text from the HTML content
+    text = soup.get_text()
+
+    # Tokenize the text into words
+    tokens = word_tokenize(text)
+
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token.lower() not in stop_words]
+
+    # Count the frequency of each token
+    token_freq = nltk.FreqDist(tokens)
+
+    return token_freq
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
