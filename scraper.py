d794d72c4241b0cd5c3db4432ced2d55cdc47699
Vini Patel <vinip@uci.edu>
2023-04-28 21:45:28 -0700

    changes

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import hashlib
 
 crawled_URLs = set()
 #hash values on all websites
@@ -26,6 +27,7 @@ def tokenize(text):
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
+
 #detects crawler traps
 def trap_detection(soup):
     tokenized_words = tokenize(soup.getText())
@@ -109,7 +111,7 @@ def is_valid(url):
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
                 + r"|png|tiff?|mid|mp2|mp3|mp4"
                 + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                + r"|ps|eps|tex|ppt|ppsx|pptx|doc|docx|xls|xlsx|names"
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 + r"|epub|dll|cnf|tgz|sha1"
                 + r"|thmx|mso|arff|rtf|jar|csv"
