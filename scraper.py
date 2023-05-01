981140fd8e470db879ecce80fd44098b9b1ab8de
Vini Patel <vinip@uci.edu>
2023-04-30 22:26:30 -0700

    Fixed everything- It should run now

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import hashlib
-import requests
+
 
 crawled_URLs = set()
 #hash values on all websites
@@ -66,18 +66,18 @@ def extract_next_links(url, resp):
         crawled_URLs.add(check_URL)
 
     #The status between 200 and 202 are good for crawling.
-    #if crawled == False and is_valid(url) and resp.status >= 200 and resp.status <= 202:  #Use this line for crawler
-    if crawled == False and is_valid(url) and resp.status_code >= 200 and resp.status_code <= 202:
+    if crawled == False and is_valid(url) and resp.status >= 200 and resp.status <= 202:  #Use this line for crawler
+    #if crawled == False and is_valid(url) and resp.status_code >= 200 and resp.status_code <= 202:
         with open("nextLink.txt", "a") as nextLinkFile:
 
-            #html_doc = resp.raw_response.content    #use this line for crawler
-            html_doc = resp.content
+            html_doc = resp.raw_response.content    #use this line for crawler
+            #html_doc = resp.content
             soup = BeautifulSoup(html_doc, 'html.parser')
             #checks to see if hash_value already exists in all_website_hash
             #tokenize function
             if(trap_detection(soup)):
                 content_tokenized = tokenize(soup.getText())
-                print(content_tokenized)
+                #print(content_tokenized)
                 #adds up total word cord from URL
                 for value in content_tokenized.values():
                     total_word_count+=value
@@ -88,7 +88,7 @@ def extract_next_links(url, resp):
                         contentFile.write(url + '\n' + str(total_word_count) + '\n')
 
                     with open("URLcontentfile.txt", "a") as URLcontentFile:
-                        text = soup.get_text().split()
+                        text = soup.get_text()
                         tokens = word_tokenize(text)
                         with open("stopwords.txt") as stopwordfile:
                             # Remove stop words
@@ -159,12 +159,12 @@ def is_valid(url):
 
 
 
-URL = "http://www.stat.uci.edu"
-response = requests.get(URL)
-#print(tokenize(URL))
+# URL = "http://www.stat.uci.edu"
+# response = requests.get(URL)
+# #print(tokenize(URL))
 
-link = scraper(URL, response)
-for links in link:
-    print(links)
-print(len(link))
+# link = scraper(URL, response)
+# for links in link:
+#     print(links)
+# print(len(link))
 
